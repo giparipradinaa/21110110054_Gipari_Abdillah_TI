436af80e1aefa2e3c08df9c1883fce44d5e249f6
Gipari Abdillah <gipari.abdillah@anteraja.id>
2022-11-19 13:29:17 +0700

    first commit

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php   
+
+$databaseHost = 'localhost';
+$databaseName = 'db_paswa';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+?>
